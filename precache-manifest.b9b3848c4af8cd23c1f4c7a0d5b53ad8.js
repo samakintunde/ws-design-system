3cd240b250da93297c2929edfb5475acead3f267
Samuel Akintunde <samakintunde37@gmail.com>
2018-12-04 11:20:46 +0100

    Updates

---
+++ precache-manifest.b9b3848c4af8cd23c1f4c7a0d5b53ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6c8920d92f4eff8f2c8",
+    "url": "/ws-design-system/static/js/runtime~main.e6c8920d.js"
+  },
+  {
+    "revision": "1e2b62ae10f6916bf06a",
+    "url": "/ws-design-system/static/js/main.1e2b62ae.chunk.js"
+  },
+  {
+    "revision": "f73b669662b7e9eb6673",
+    "url": "/ws-design-system/static/js/1.f73b6696.chunk.js"
+  },
+  {
+    "revision": "1e2b62ae10f6916bf06a",
+    "url": "/ws-design-system/static/css/main.d6218a02.chunk.css"
+  },
+  {
+    "revision": "bd4e7690a22680043d89303ebfd18c2c",
+    "url": "/ws-design-system/index.html"
+  }
+];
